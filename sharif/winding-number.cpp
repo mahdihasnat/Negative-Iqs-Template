98551b0c3cd9c16354c12791b796e545b12efeb5
IAMSHARIFUL <1705119@ugrad.cse.buet.ac.bd>
2022-09-07 03:09:20 +0600

    sharif added

---
+++ sharif/winding-number.cpp
@@ -0,0 +1,62 @@
+const double eps = 1e-9;
+const double PI = acos((double)-1.0);
+
+int sign(double x) { return (x > eps) - (x < -eps); }
+struct PT {
+    double x, y;
+    PT() { x = 0, y = 0; }
+    PT(double x, double y) : x(x), y(y) {}
+    PT(const PT &p) : x(p.x), y(p.y)    {}
+    PT operator + (const PT &a) const { return PT(x + a.x, y + a.y); }
+    PT operator - (const PT &a) const { return PT(x - a.x, y - a.y); }
+    PT operator * (const double a) const { return PT(x * a, y * a); }
+    friend PT operator * (const double &a, const PT &b) { return PT(a * b.x, a * b.y); }
+    PT operator / (const double a) const { return PT(x / a, y / a); }
+    bool operator == (PT a) const { return sign(a.x - x) == 0 && sign(a.y - y) == 0; }
+    bool operator != (PT a) const { return !(*this == a); }
+    bool operator < (PT a) const { return sign(a.x - x) == 0 ? y < a.y : x < a.x; }
+    bool operator > (PT a) const { return sign(a.x - x) == 0 ? y > a.y : x > a.x; }
+    double norm() { return sqrt(x * x + y * y); }
+    double norm2() { return x * x + y * y; }
+    PT perp() { return PT(-y, x); }
+    double arg() { return atan2(y, x); }
+    PT truncate(double r) { // returns a vector with norm r and having same direction
+        double k = norm();
+        if (!sign(k)) return *this;
+        r /= k;
+        return PT(x * r, y * r);
+    }
+};
+inline double cross(PT a, PT b) { return a.x * b.y - a.y * b.x; }
+inline int orientation(PT a, PT b, PT c) { return sign(cross(b - a, c - a)); }
+bool is_point_on_seg(PT a, PT b, PT p) {
+    if (fabs(cross(p - b, a - b)) < eps) {
+        if (p.x < min(a.x, b.x) || p.x > max(a.x, b.x)) return false;
+        if (p.y < min(a.y, b.y) || p.y > max(a.y, b.y)) return false;
+        return true;
+    }
+    return false;
+}
+
+bool is_point_on_polygon(vector<PT> &p, const PT& z) {
+    int n = p.size();
+    for (int i = 0; i < n; i++) {
+    	if (is_point_on_seg(p[i], p[(i + 1) % n], z)) return 1;
+    }
+    return 0;
+}
+// returns 1e9 if the point is on the polygon
+int winding_number(vector<PT> &p, const PT& z) { // O(n)
+    if (is_point_on_polygon(p, z)) return 1e9;
+    int n = p.size(), ans = 0;
+    for (int i = 0; i < n; ++i) {
+        int j = (i + 1) % n;
+        bool below = p[i].y < z.y;
+        if (below != (p[j].y < z.y)) {
+            auto orient = orientation(z, p[j], p[i]);
+            if (orient == 0) return 0;
+            if (below == (orient > 0)) ans += below ? 1 : -1;
+        }
+    }
+    return ans;
+}
