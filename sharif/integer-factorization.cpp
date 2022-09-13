4ba3367aaae7ee05d8965bf6a4054b65efec8c29
IAMSHARIFUL <1705119@ugrad.cse.buet.ac.bd>
2022-09-14 00:31:09 +0600

    Integer Factorization Added

---
+++ sharif/integer-factorization.cpp
@@ -0,0 +1,87 @@
+long long pollards_p_minus_1(long long n) {
+    int B = 10;
+    long long g = 1;
+    while (B <= 1000000 && g < n) {
+        long long a = 2 + rand() %  (n - 3);
+        g = gcd(a, n);
+        if (g > 1)
+            return g;
+
+        // compute a^M
+        for (int p : primes) {
+            if (p >= B)
+                continue;
+            long long p_power = 1;
+            while (p_power * p <= B)
+                p_power *= p;
+            a = power(a, p_power, n);
+
+            g = gcd(a - 1, n);
+            if (g > 1 && g < n)
+                return g;
+        }
+        B *= 2;
+    }
+    return 1;
+}
+
+long long mult(long long a, long long b, long long mod) {
+    long long result = 0;
+    while (b) {
+        if (b & 1)
+            result = (result + a) % mod;
+        a = (a + a) % mod;
+        b >>= 1;
+    }
+    return result;
+}
+
+long long f(long long x, long long c, long long mod) {
+    return (mult(x, x, mod) + c) % mod;
+}
+
+long long rho(long long n, long long x0=2, long long c=1) {
+    long long x = x0;
+    long long y = x0;
+    long long g = 1;
+    while (g == 1) {
+        x = f(x, c, n);
+        y = f(y, c, n);
+        y = f(y, c, n);
+        g = gcd(abs(x - y), n);
+    }
+    return g;
+}
+
+long long brent(long long n, long long x0=2, long long c=1) {
+    long long x = x0;
+    long long g = 1;
+    long long q = 1;
+    long long xs, y;
+
+    int m = 128;
+    int l = 1;
+    while (g == 1) {
+        y = x;
+        for (int i = 1; i < l; i++)
+            x = f(x, c, n);
+        int k = 0;
+        while (k < l && g == 1) {
+            xs = x;
+            for (int i = 0; i < m && i < l - k; i++) {
+                x = f(x, c, n);
+                q = mult(q, abs(y - x), n);
+            }
+            g = gcd(q, n);
+            k += m;
+        }
+        l *= 2;
+    }
+    if (g == n) {
+        do {
+            xs = f(xs, c, n);
+            g = gcd(abs(xs - y), n);
+        } while (g == 1);
+    }
+    return g;
+}
