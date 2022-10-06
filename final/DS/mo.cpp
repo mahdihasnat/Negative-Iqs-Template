31cfdb82a2ea72a2a0bc82b544527cd0edae9d5c
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-10-06 16:22:05 +0600

    added ds

---
+++ final/DS/mo.cpp
@@ -0,0 +1,36 @@
+struct Query {
+    int l, r,k, idx;
+    bool operator<(Query other) const
+    {
+        if(l/block_size!=other.l/block_size) return (l<other.l);
+        return (l/block_size&1)? (r<other.r) : (r>other.r);
+    }
+};
+vector<int> mo_s_algorithm(vector<Query> queries) {
+    vector<int> answers(queries.size());
+    sort(queries.begin(), queries.end());
+    // TODO: initialize data structure
+    int cur_l = 0;
+    int cur_r = -1;
+    // invariant: data structure will always reflect the range [cur_l, cur_r]
+    for (Query q : queries) {
+        while (cur_l > q.l) {
+            cur_l--;
+            add(cur_l);
+        }
+        while (cur_r < q.r) {
+            cur_r++;
+            add(cur_r);
+        }
+        while (cur_l < q.l) {
+            remove(cur_l);
+            cur_l++;
+        }
+        while (cur_r > q.r) {
+            remove(cur_r);
+            cur_r--;
+        }
+        answers[q.idx] = get_answer();
+    }
+    return answers;
+}
