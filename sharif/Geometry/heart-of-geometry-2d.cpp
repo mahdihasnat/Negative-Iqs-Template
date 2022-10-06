3ada7c1af76668983ff92e62655674c51e54ba33
IAMSHARIFUL <95834467+IAMSHARIFUL@users.noreply.github.com>
2022-10-06 11:13:20 +0600

    Update heart-of-geometry-2d.cpp

--- sharif/Geometry/heart-of-geometry-2d.cpp
+++ sharif/Geometry/heart-of-geometry-2d.cpp
@@ -109,15 +109,18 @@ bool cmp(pt a, pt b)
     return a.x < b.x || (a.x == b.x && a.y < b.y);
 }
 
-bool cw(pt a, pt b, pt c)
-{
-    return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) < 0;
+bool ccw(pt a, pt b, pt c, bool include_collinear=false) {
+    int o = orientation(a, b, c);
+    return o > 0 || (include_collinear && o == 0);
 }
 
-bool ccw(pt a, pt b, pt c)
-{
-    return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) > 0;
+
+bool cw(pt a, pt b, pt c, bool include_collinear=false) {
+    int o = orientation(a, b, c);
+    return o < 0 || (include_collinear && o == 0);
 }
+bool collinear(pt a, pt b, pt c) { return orientation(a, b, c) == 0; }
+
 
 double area(pt a, pt b, pt c)
 {
@@ -486,35 +489,36 @@ double circle_polygon_intersection(circle c,vector<pt> &V)
     return ans;
 }
 
-void convex_hull(vector<pt>& a) {
-    if (a.size() == 1)
-        return;
-
-    sort(a.begin(), a.end(), &cmp);
-    pt p1 = a[0], p2 = a.back();
-    vector<pt> up, down;
-    up.push_back(p1);
-    down.push_back(p1);
-    for (int i = 1; i < (int)a.size(); i++) {
-        if (i == a.size() - 1 || cw(p1, a[i], p2)) {
-            while (up.size() >= 2 && !cw(up[up.size()-2], up[up.size()-1], a[i]))
-                up.pop_back();
-            up.push_back(a[i]);
-        }
-        if (i == a.size() - 1 || ccw(p1, a[i], p2)) {
-            while(down.size() >= 2 && !ccw(down[down.size()-2], down[down.size()-1], a[i]))
-                down.pop_back();
-            down.push_back(a[i]);
-        }
+void convex_hull(vector<pt>& a, bool include_collinear = false) {
+    pt p0 = *min_element(a.begin(), a.end(), [](pt a, pt b) {
+        return make_pair(a.y, a.x) < make_pair(b.y, b.x);
+    });
+    sort(a.begin(), a.end(), [&p0](const pt& a, const pt& b) {
+        int o = orientation(p0, a, b);
+        if (o == 0)
+            return (p0.x-a.x)*(p0.x-a.x) + (p0.y-a.y)*(p0.y-a.y)
+                < (p0.x-b.x)*(p0.x-b.x) + (p0.y-b.y)*(p0.y-b.y);
+        return o < 0;
+    });
+    if (include_collinear) {
+        int i = (int)a.size()-1;
+        while (i >= 0 && collinear(p0, a[i], a.back())) i--;
+        reverse(a.begin()+i+1, a.end());
     }
-    a.clear();
-    for (int i = 0; i < (int)up.size(); i++)
-        a.push_back(up[i]);
-    for (int i = down.size() - 2; i > 0; i--)
-        a.push_back(down[i]);
-    int n = a.size();
-    for(int i=0; i < n - 1 - i; i++)
-        swap(a[i],a[n-1-i]);
+
+    vector<pt> st;
+    for (int i = 0; i < (int)a.size(); i++) {
+        while (st.size() > 1 && !cw(st[st.size()-2], st.back(), a[i], include_collinear))
+            st.pop_back();
+        st.push_back(a[i]);
+    }
+
+    a = st;
+    int m = a.size();
+    for(int i = 0;i<m-1-i;i++)
+    {
+        swap(a[i],a[m-1-i]);
+    }
 }
 
 double mindist;
